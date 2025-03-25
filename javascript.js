8067ae1e985f79cc94940193a7c04a17356c38f3
Tyler <179739422+CodeCowboy96@users.noreply.github.com>
2025-03-25 18:33:37 -0500

    Cleanup js code/notes and create to do list

--- javascript.js
+++ javascript.js
@@ -1,10 +1,21 @@
-//1.import logic for the 4 cardinal math expressions
+//Bugs to fix:
+    //1. needs to only evaluate a single pair of numbers at a time 
+        //currently if you enter 12+6+8 the program returns 14 because it doesn't store 12 only the last 2 numbers
+        //should solve for 12+6 then move on to the sum+8=26
+    //2.When a result is displayed, entering a new number should start a new calculation
+        //currently new numbers append to the end of displayed number. 
+//Additions to create
+    //1. Add a decimal button to allow for decimal operations
+        //make sure only one decimal allowed at a time
+    //2. Add a backspace button 
+    //3. Add keyboard support
+
+
 //Calculator Logic
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
-const divide = (a, b) => b !== 0 ? a / b : "Error";
-
+const divide = (a, b) => b !== 0 ? a / b : "Not happening bro";
 
 let firstNum = null;
 let secondNum = null;
@@ -37,7 +48,7 @@ buttons.forEach(button => {
       updateDisplay(buttonValue);
     });
 });
-
+//Functions for button handler
 function updateDisplay(value) {
     if (display.textContent === '0' || shouldResetDisplay) {
       display.textContent = value;
@@ -59,7 +70,18 @@ function updateDisplay(value) {
       display.textContent = operate(currentOperator, firstNum, secondNum);
       firstNum = null; 
     }
+    function operate(operator, a, b) {
+        switch(operator) {
+          case '+': return add(a, b);
+          case '-': return subtract(a, b);
+          case '*': return multiply(a, b);
+          case '/': return divide(a, b);
+          default: return b;  // Fallback to current display value
+        }
+      }
   }
+
+  
 
   function clearCalculator() {
     display.textContent = '0';
@@ -67,18 +89,3 @@ function updateDisplay(value) {
     secondNum = null;
     currentOperator = null;
   }
-
-  function operate(operator, a, b) {
-    switch(operator) {
-      case '+': return add(a, b);
-      case '-': return subtract(a, b);
-      case '*': return multiply(a, b);
-      case '/': return divide(a, b);
-      default: return b;  // Fallback to current display value
-    }
-  }
-//2.create UI through java/css
-    //should include clear button
-    //don't let decimals roll over the display
-
-//Button Grid
